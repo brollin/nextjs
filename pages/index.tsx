10e8c52471545df64fa80bb915b768714ca094ab
Ben Rollin <ben.rollin@clever.com>
2022-06-08 17:45:59 -0700

    make planets brighter

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ export default function HappyBirthdayPlanets() {
       </Head>
       <svg width="100%" height={height} viewBox={`0 0 ${width} ${height}`}>
         <defs>
-          <radialGradient id="planetGradient" cx="0.5" cy="0.5" r="0.5" fx="0.25" fy="0.25">
+          <radialGradient id="planetGradient" cx="0.5" cy="0.5" r="0.55" fx="0.25" fy="0.25">
             <stop offset="0%" stopColor={planetColorGradient(planet.temperature / 1300)} />
             <stop offset="100%" stopColor="black" />
           </radialGradient>
