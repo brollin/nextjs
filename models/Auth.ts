661e16c7db64886eacc5544022e51ab9013d9f52
Ben Rollin <ben.rollin@clever.com>
2023-01-07 22:59:15 -0800

    separate me from auth state

--- models/Auth.ts
+++ models/Auth.ts
@@ -19,36 +19,36 @@ export class Auth {
     onAccessTokenExpiry: (refreshAccessToken) => refreshAccessToken(),
     onInvalidGrant: console.warn,
   });
-  me?: Me;
 
-  init = async () => {
+  init = async (setMe: (me: Me) => void) => {
     try {
       const accessContext = await this.oauth.getAccessToken();
-      if (accessContext) await this.authenticate();
+      if (accessContext) await this.authenticate(setMe);
     } catch (err) {
       console.error(err);
     }
-    if (!this.me) {
-      try {
-        const hasAuthCode = await this.oauth.isReturningFromAuthServer();
-        if (hasAuthCode) await this.authenticate();
-      } catch (err) {
-        console.error(err);
-      }
+
+    try {
+      const hasAuthCode = await this.oauth.isReturningFromAuthServer();
+      if (hasAuthCode) await this.authenticate(setMe);
+    } catch (err) {
+      console.error(err);
     }
+
+    // remove the state and code from the query parameters
+    window.history.replaceState(null, null, window.location.pathname);
   };
 
   login = async () => {
     await this.oauth.fetchAuthorizationCode();
   };
 
-  logout = async () => {
-    if (this.me) await this.me.httpClient(`${lichessHost}/api/token`, { method: "DELETE" });
+  logout = async (me: Me) => {
+    await me.httpClient(`${lichessHost}/api/token`, { method: "DELETE" });
     localStorage.clear();
-    this.me = undefined;
   };
 
-  private authenticate = async () => {
+  private authenticate = async (setMe: (me: Me) => void) => {
     const httpClient = this.oauth.decorateFetchHTTPClient(window.fetch);
     const res = await httpClient(`${lichessHost}/api/account`);
     const me = {
@@ -56,17 +56,17 @@ export class Auth {
       httpClient,
     };
     if (me.error) throw me.error;
-    this.me = me;
+    setMe(me);
   };
 
-  fetchBody = async (path: string, config: any = {}) => {
-    const res = await this.fetchResponse(path, config);
+  fetchBody = async (me: Me, path: string, config: any = {}) => {
+    const res = await this.fetchResponse(me, path, config);
     const body = await res.json();
     return body;
   };
 
-  private fetchResponse = async (path: string, config: any = {}) => {
-    const res = await (this.me?.httpClient || window.fetch)(`${lichessHost}${path}`, config);
+  private fetchResponse = async (me: Me, path: string, config: any = {}) => {
+    const res = await (me.httpClient || window.fetch)(`${lichessHost}${path}`, config);
     if (res.error || !res.ok) {
       const err = `${res.error} ${res.status} ${res.statusText}`;
       alert(err);
--- pages/chess.tsx
+++ pages/chess.tsx
@@ -1,33 +1,44 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { Auth } from "../models/Auth";
+import { useEffect, useRef, useState } from "react";
+import { Auth, Me } from "../models/Auth";
 import styles from "../styles/Chess.module.css";
 
 export default function Chess() {
   const [auth, setAuth] = useState<Auth | null>(null);
+  const [me, setMe] = useState<Me | null>(null);
+  const initialized = useRef(false);
 
   useEffect(() => {
+    if (initialized.current) return;
+    initialized.current = true;
+
     const initAuth = async () => {
       const newAuth = new Auth();
-      await newAuth.init();
+      await newAuth.init(setMe);
       setAuth(newAuth);
     };
     initAuth();
   }, []);
 
-  const { me } = auth;
   return (
     <div className={styles.container}>
       <Head>
         <title>Brollin Chess Space</title>
       </Head>
       <h1 className={styles.title}>Hello world</h1>
-      {me ? (
-        <button onClick={auth?.logout}>Logout</button>
-      ) : (
-        <button disabled={!auth} onClick={auth?.login}>
-          Login with Lichess
+      {!auth ? (
+        <div>loading</div>
+      ) : me ? (
+        <button
+          onClick={() => {
+            auth.logout(me);
+            setMe(null);
+          }}
+        >
+          Logout
         </button>
+      ) : (
+        <button onClick={auth.login}>Login with Lichess</button>
       )}
     </div>
   );
