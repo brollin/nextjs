47368210c471c9c0523fec1bec2ba67d4db78532
Ben Rollin <ben.rollin@clever.com>
2024-01-14 17:43:46 -0800

    landing page styling

--- modules/landing/Landing.tsx
+++ modules/landing/Landing.tsx
@@ -1,21 +1,23 @@
 import Link from "next/link";
-import styles from "@/styles/Planets.module.css";
+import Head from "next/head";
+import { VStack } from "@chakra-ui/react";
+import { MdConstruction } from "react-icons/md";
 
 export default function Landing() {
   return (
-    <div>
-      <div className={styles.link}>
-        <Link href="/capitalizer">capitalize</Link>
-      </div>
-      <div className={styles.link}>
-        <Link href="/cosmere">cosmere</Link>
-      </div>
-      <div className={styles.link}>
+    <>
+      <Head>
+        <title>beep boop</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <VStack width="100vw" height="100vh" justify="center">
+        <MdConstruction size={200} />
+        <Link href="/capitalizer">capitalizer</Link>
+        <Link href="/health">health</Link>
         <Link href="/sandbox">sandbox</Link>
-      </div>
-      <div className={styles.link}>
         <Link href="/chess">chess</Link>
-      </div>
-    </div>
+      </VStack>
+    </>
   );
 }
