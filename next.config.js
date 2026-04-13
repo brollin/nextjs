f39064663aebb2a36f8f52e52fba491fea8f2215
Ben Rollin <ben.rollin@clever.com>
2026-04-13 14:33:24 -0700

    health redirect for old health link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/health",
+        destination: "https://health.brollin.space",
+        permanent: true,
+      },
+    ];
+  },
   webpack: (config, options) => {
     // Use glslify-loader for GLSL files
     config.module.rules.push({ test: /\.(glsl|vs|fs|vert|frag)$/, use: ["raw-loader", "glslify-loader"] });
